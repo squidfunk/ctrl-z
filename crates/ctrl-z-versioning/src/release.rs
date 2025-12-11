6f466a3d4992afea877b53ad288990ebccddab33
squidfunk <martin.donath@squidfunk.com>
2025-12-11 13:05:09 +0100

    fix: something

--- crates/ctrl-z-versioning/src/release.rs
+++ crates/ctrl-z-versioning/src/release.rs
@@ -120,7 +120,7 @@ where
             }
         };
 
-        // testss
+        // testssss
 
         // get summary from repository and add to changeset.
         // get canonical version – how? workspace config
